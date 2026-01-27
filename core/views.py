7e1bdfa5cf7e16df38ab337039485193c4e717b9
Dahiru Aliyu Adamu <dahirualiyuadamu@gmail.com>
2026-01-27 06:14:23 +0100

    Day 5: Implement blog views, URLs, and templates with dynamic content

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def home(request):
-    return render(request, 'core/home.html')
+    latest_posts = Post.objects.filter(status='PUBLISHED').order_by('-published_at')[:3]
+    return render(request, 'core/home.html', {'latest_posts': latest_posts})
 
 def about(request):
     return render(request, 'core/about.html')
